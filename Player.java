37339ab89ffd4532dc596970a96835a60f47b146
Oliver Efthemes <145041870+OliverEf@users.noreply.github.com>
2025-01-28 23:02:08 +0000

    Implement initial structure for Main, Player, and Game classes with unimplemented methods

---
+++ Player.java
@@ -0,0 +1,36 @@
+public class Player implements Person{
+    
+    public Player() {
+
+    }
+
+    @Override
+    public void setName(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setName'");
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
+    @Override
+    public int getHeight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHeight'");
+    }
+
+    @Override
+    public int getAge() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAge'");
+    }
+
+    @Override
+    public Results getResults() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getResults'");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,34 @@ public class Main implements Spec{
     public static void main(String[] args) {
         
     }
+
+    @Override
+    public String askString(String question) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'askString'");
+    }
+
+    @Override
+    public int askNumber(String question) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'askNumber'");
+    }
+
+    @Override
+    public Results getResults(Game guessingOrBetter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getResults'");
+    }
+
+    @Override
+    public Person makePerson(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'makePerson'");
+    }
+
+    @Override
+    public void addResults(Person player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addResults'");
+    }
 }
